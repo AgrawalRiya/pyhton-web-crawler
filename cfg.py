4129434c63a2b3459f7c4ab0c27dde90a63d86f7
Riya Agrawal <69007467+AgrawalRiya@users.noreply.github.com>
2021-06-11 16:02:22 +0530

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,6 @@
+main.py        -  main function
+database.py    -  Contains function related to database operations
+crawler.py     -  Contains function related to validlinks, saving file, crawling
+
+URL            -  'https://flinkhub.com'
+'crawler'      -  MongoDB Database
